57572b0bf4203f77291697e5a87024921267b04b
Phú <heoquay456789@gmail.com>
2020-04-21 11:31:17 +0700

    first commit

---
+++ src/codes/test.java
@@ -0,0 +1,41 @@
+package codes;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("moi nhap so : ");
+      int n = scanner.nextInt();
+      if (KiemTraSoHoanHao(n)==true) {
+    	  System.out.println("day la so hoan hao");
+      }
+      else {
+    	  System.out.println("day khong phai la so hoan hao");
+      }
+	}
+     public static boolean KiemTraSoHoanHao (int n) {
+    	int tong=0;
+    	for( int i=1;i<=(n/2);i++) {
+    		if(n%i==0) tong+=i;{
+    		}
+    		if (tong==n) return true;
+    		}
+    	return false;
+     }
+}
+    	
+    	
+    
+    		
+ 
+    	
+    		
+    
+        
+        
+	
+
+
