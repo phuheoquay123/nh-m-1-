ae723a97a25273bfc31b61016f607804b2d61e98
NTL7D <55150110+NTL7D@users.noreply.github.com>
2020-04-21 11:52:14 +0700

    Update test.java

--- src/codes/test.java
+++ src/codes/test.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
       Scanner scanner = new Scanner(System.in);
       System.out.println("moi nhap so : ");
       int n = scanner.nextInt();
