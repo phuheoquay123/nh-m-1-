6df0ced061146e82e086dfec8060f2dea45f5cc1
phuheoquay123 <63390154+phuheoquay123@users.noreply.github.com>
2020-05-08 10:16:29 +0700

    Update Thongtinsinhvien.java

--- Thongtinsinhvien.java
+++ Thongtinsinhvien.java
@@ -1,23 +1,25 @@
 package com.baithicuoiki.sinhvien;
 import java.util.Scanner;
-
 import nhapmaSV.nhapMaSv;
-
 public class Thongtinsinhvien {
 	public static void thongtinsinhvien() {
 		String fullName="";
 		int ngay, thang, nam, tuoi;
 		String MSSV;
 		int namhientai=2020; 
-	    Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		System.out.print("Moi nhap ho va ten sinh vien: ");
 		fullName=scanner.nextLine();
-		System.out.print("Moi nhap ngay sinh: ");
-		ngay=Integer.parseInt(scanner.nextLine());
-		System.out.print("Moi nhap thang sinh: ");
-		thang=Integer.parseInt(scanner.nextLine());
-		System.out.print("Moi nhap nam sinh: ");
-		nam=Integer.parseInt(scanner.nextLine());
+		if (nhapMaSv.nhaphovaten(fullName)==false) {
+			System.out.println("Ho va ten khong hop le, vui long kiem tra lai");
+		}
+		else {
+			System.out.print("Moi nhap ngay sinh: ");
+		        ngay=Integer.parseInt(scanner.nextLine());
+		        System.out.print("Moi nhap thang sinh: ");
+		        thang=Integer.parseInt(scanner.nextLine());
+		        System.out.print("Moi nhap nam sinh: ");
+		        nam=Integer.parseInt(scanner.nextLine());
 		try {
 			tuoi=namhientai-nam;
 			if(tuoi>24) {
@@ -27,23 +29,23 @@ public class Thongtinsinhvien {
 				System.out.println(fullName + " khong phai la sinh vien");
 			}
 			else {
-			System.out.println(fullName + " la sinh vien");
-			System.out.print("Moi nhap MSSV: NIIE");
-			
-		    MSSV=scanner.nextLine();
-		    if (nhapMaSv.nhapmasv(MSSV)) {
-		    	System.out.println("MSSV: NIIE" + MSSV);
-		        System.out.println("Ho va ten sinh vien: " + fullName);
-			    System.out.println("Sinh ngay " + ngay + " thang " + thang + " nam " + nam);
-			    Tinhdiemcuoiki.tinhdiemcuoiki();
-		    }
-		    else {
-		    	System.out.println("Ma sai dinh dang, vui long kiem tra lai");
-		    }
-		    }
+				System.out.print("Moi nhap MSSV: NIIE");
+				MSSV=scanner.nextLine();
+				if (nhapMaSv.nhapmasv(MSSV)) {
+					System.out.println("-------THONG TIN SINH VIEN-------");
+					System.out.println("MSSV: NIIE" + MSSV);
+					System.out.println("Ho va ten sinh vien: " + fullName);
+					System.out.println("Sinh ngay " + ngay + " thang " + thang + " nam " + nam);
+					Tinhdiemcuoiki.tinhdiemcuoiki();
+				}
+				else {
+					System.out.println("Ma sinh vien khong hop le, vui long kiem tra lai");
+				}
+			}
 			}
 		catch (Exception e) {
 			System.out.println(e);
 		}
 	}
+}
 }
