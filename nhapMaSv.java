26b51ed61119f2b67266aac88b9fa7bdbc77d8b8
phuheoquay123 <63390154+phuheoquay123@users.noreply.github.com>
2020-05-09 22:02:50 +0700

    Update nhapMaSv.java

--- nhapMaSv.java
+++ nhapMaSv.java
@@ -1,19 +1,44 @@
 package nhapmaSV;
 public class nhapMaSv {
 	public static boolean nhapmasv(String MSSV){
-			
-	        String pattern="[0-9]{10}[A-Z]{1}";
-	        if (MSSV.matches(pattern)) {
-	        	return true;
-	        }
-	        else {
-	        	return false;
-	        }
-	       
-}
+		String pattern="[0-9]{10}[A-Z]{1}";
+		if (MSSV.matches(pattern)) {
+			return true;
+	    }
+	    else {
+	        return false;
+	    }
+	}
 	public static boolean nhaphovaten(String fullName) {
 		String pattern="[a-z A-Z]+";
 		if (fullName.matches(pattern)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public static boolean nhapngaysinh(String ngay) {
+		String pattern="[0-9]{1,2}";
+		if (ngay.matches(pattern)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public static boolean nhapthangsinh(String thang) {
+		String pattern="[0-9]{1}";
+		if (thang.matches(pattern)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public static boolean nhapnamsinh(String nam) {
+		String pattern="[0-9]{4}";
+		if (nam.matches(pattern)) {
 			return true;
 		}
 		else {
